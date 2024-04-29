da2686403bfdcb6948d3e3bb1d6de523adcd5b28
kamaLc73 <126430069+kamaLc73@users.noreply.github.com>
2024-04-29 13:13:56 +0100

    agregate

---
+++ Java & JEE/Java/agregation/src/com/java/india/Auteur.java
@@ -0,0 +1,46 @@
+package com.java.india;
+
+public class Auteur {
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	public Auteur() {
+	}
+
+	public Auteur(int id, String nom, String prenom) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String toString() {
+		return "Auteur [id = " + id + ", nom = " + nom + ", prenom = " + prenom + "]";
+	}
+
+	
+}
