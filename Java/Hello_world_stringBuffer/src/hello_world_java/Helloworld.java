39dfb8dfccd44f52dfd8b39a83d91490a2bac2ac
kamaLc73 <126430069+kamaLc73@users.noreply.github.com>
2024-04-29 13:35:39 +0100

    hello_world

---
+++ Java/Hello_world_stringBuffer/src/hello_world_java/Helloworld.java
@@ -0,0 +1,10 @@
+package hello_world_java;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world of JAVA ");
+
+	}
+
+}
---
+++ Java/Hello_world_stringBuffer/src/tst/stringTest.java
@@ -0,0 +1,16 @@
+package tst;
+
+public class stringTest {
+    public static void main(String[] args) {
+    	
+        String str = new String("test_JAVA");
+        
+        System.out.println(str);
+        
+        System.out.println(str.length());
+        
+        System.out.println(str.toLowerCase());
+        
+        System.out.println(str.contains("JAVA"));
+    }
+}
