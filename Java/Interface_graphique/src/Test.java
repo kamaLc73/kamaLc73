9b81105807949617b82121438f95ae32cdd47b6d
kamaLc73 <126430069+kamaLc73@users.noreply.github.com>
2024-04-29 13:46:20 +0100

    Interface

---
+++ Java/Interface_graphique/src/Test.java
@@ -0,0 +1,13 @@
+package com.java.india;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int bin = 0xf9;
+		System.out.println(bin);
+		String msg = "kamal";
+		System.out.println(msg.substring(2, 4));
+
+	}
+
+}
---
+++ Java/Interface_graphique/src/labelTextField.java
@@ -0,0 +1,30 @@
+package com.java.india;
+
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class labelTextField {
+
+    public static JPanel creeLabelTextField (String txt, int col) {
+    	JPanel p = new JPanel();
+    	JLabel l = new JLabel(txt);
+    	p.add(l);
+        JTextField t = new JTextField(col);
+        p.add(t);
+        return p ;
+    }
+    public static JPanel creeButton(String txt) {
+    	JPanel p = new JPanel();
+    	JButton b = new JButton(txt);
+    	p.add(b);
+    	return p;
+    	
+	}
+    
+    
+    
+    
+}
